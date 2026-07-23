f8399639fc03ce797c6b4d583c6cb37045237938
Kaisarea Tennisi <kaisareatennisi3@gmail.com>
2026-07-23 20:35:14 +0700

    salah guys

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,26 +1,114 @@
+const { User, Task, Post } = require('../models/index')
+const bcrypt = require('bcrypt');
+const post = require('../models/post');
+
 class Controller {
 
     static async homePage(req, res){
         try {
+            res.render('homePage')
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async getRegister(req, res) {
+        try {
+            res.render('register', { error: req.query.error || null });
+        } catch (error) {
+            res.send(error);
+        }
+    }
+
+    static async studentDashboard(req, res) {
+        try {
+            const tasks = await Task.findAll({ where: { userId: req.session.userId } });
+            let user = await User.findByPk(req.session.userId )
+
+            console.log(user);
+
+            console.log(tasks);
+            res.render('dashboard', { tasks, role: 'student' , user});
+        } catch (error) {
+            res.send(error);
+        }
+    }
+    
+    static async adminDashboard(req, res) {
+        try {
+            res.send('Admin')
+            // const tasks = await Task.findAll({ include: 'User' }); // semua task, semua user
+            // res.render('dashboard', { tasks, role: 'admin' });
+        } catch (error) {
+            res.send(error);
+        }
+    }
+    
+    static async manageUsers(req, res) {
+        try {
+            const users = await User.findAll();
+            res.render('manageUsers', { users });
+        } catch (error) {
+            res.send(error);
+        }
+    }
+    
+    static async postRegister(req, res) {
+        try {
+            const { name, email, password, role } = req.body;
+
+            await User.create({ name, email, password, role});
+
+            res.redirect('/login');
+        } catch (error) {
+            if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+                return res.redirect(`/register?error=${encodeURIComponent(error.errors[0].message)}`);
+            }
+            res.send(error);
+        }
+    }
+    
+    static async login(req, res) {
+        try {
+            res.render('login', { error: req.query.error || null }); 
+        } catch (error) {
+            res.send(error);
+        }
+    }
+    
+    static async postLogin(req, res) {
+        try {
+            const { email, password } = req.body;
+            const user = await User.findOne({ where: { email } });
+            const users = await User.findAll()
+
+            // console.log(user, "<<<");
+            // console.log(users);
+    
+            if (!user || !(await user.comparePassword(password))) {
+                return res.redirect('/login?error=Email atau password salah'); // Pakai return
+            }
+    
+            req.session.userId = user.id;
+            req.session.role = user.role;
+    
+            if (user.role === 'admin') {
+                return res.redirect('/admin'); // Pakai return
+            }
             
-        } catch (error) {
-            res.send(error)
+            return res.redirect('/student'); // Pakai return
+        } catch (err) {
+            console.log(err);
+            res.send(err);
         }
     }
-
-    static async login(req, res){
+    
+    static async logout(req, res) {
         try {
-            res.render('login')
+            req.session.destroy();
+            res.redirect('/login');
         } catch (error) {
-            res.send(error)
-        }
-    }
-
-    static async signUp(req, res){
-        try {
-            res.render('signUp')
-        } catch (error) {
-            res.send(error)
+            res.send(error);
         }
     }
 
@@ -34,8 +122,11 @@ class Controller {
 
     static async socialFeed(req, res){
         try {
+            const post = await Post.findAll({ where: { userId: req.session.userId } });
+            let user = await User.findByPk(req.session.userId )
             res.render('socialFeed')
         } catch (error) {
+            console.log(error);
             res.send(error)
         }
     }
@@ -130,7 +221,7 @@ class Controller {
 
     static async profile(req, res){
         try {
-            res.render('profile')
+            
         } catch (error) {
             res.send(error)
         }
