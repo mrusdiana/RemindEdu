857d6fad4135b09ca3d1c15d0be271abd4a3087b
M Rusdiana <muhamadrusdiana452@gmail.com>
2026-07-23 17:49:59 +0700

    loginDone

--- seeders/20260723052207-seed-users.js
+++ seeders/20260723052207-seed-users.js
@@ -1,20 +1,20 @@
 'use strict';
 const fs = require('fs').promises
+const bcrypt = require('bcryptjs'); 
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
- 
-    let users = JSON.parse(await fs.readFile('./data/users.json', 'utf8'))
+    let users = JSON.parse(await fs.readFile('./data/users.json', 'utf8'));
 
     users.forEach(user => {
-      
-      delete user.id
-      user.createdAt = new Date()
-      user.updatedAt = new Date()
+      delete user.id;
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10)); 
+      user.createdAt = new Date();
+      user.updatedAt = new Date();
     });
 
-    await queryInterface.bulkInsert('Users', users)
+    await queryInterface.bulkInsert('Users', users);
   },
 
   async down (queryInterface, Sequelize) {
