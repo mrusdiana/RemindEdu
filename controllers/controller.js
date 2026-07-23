e5b38a45a9b55da280d78cdec07912f19d7ec9de
Kaisarea Tennisi <kaisareatennisi3@gmail.com>
2026-07-23 15:34:18 +0700

    html & css login

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,7 +10,7 @@ class Controller {
 
     static async login(req, res){
         try {
-            
+            res.render('login')
         } catch (error) {
             res.send(error)
         }
